ce18459501e9997e8cde3b130331f16b5bb8093a
Noorjahan Dudekula <90750848+Noorjahan153@users.noreply.github.com>
2026-02-18 13:28:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,24 +2,56 @@ provider "aws" {
   region = var.region
 }
 
-################ DEFAULT VPC ################
+################ VPC ################
 
-data "aws_vpc" "default" {
-  default = true
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
 }
 
-data "aws_subnets" "all" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-2a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-2b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
+}
+
+resource "aws_route_table_association" "a1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "a2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rt.id
 }
 
 ################ SECURITY GROUP ################
 
 resource "aws_security_group" "strapi" {
   name   = "strapi-sg"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = aws_vpc.main.id
 
   ingress {
     from_port   = 1337
@@ -100,7 +132,11 @@ resource "aws_ecs_service" "service" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = data.aws_subnets.all.ids
+    subnets = [
+      aws_subnet.subnet1.id,
+      aws_subnet.subnet2.id
+    ]
+
     security_groups = [aws_security_group.strapi.id]
     assign_public_ip = true
   }
