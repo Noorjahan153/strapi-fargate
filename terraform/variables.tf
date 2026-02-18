2bf3267645b9df79ff53abb5c94f675cfc6b0246
Noorjahan Dudekula <90750848+Noorjahan153@users.noreply.github.com>
2026-02-18 20:50:06 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,24 @@
-variable "aws_account_id" {
-  description = "AWS account ID for ECR"
-  type        = string
+variable "ecr_repo" {
+  description = "Full ECR repository URI including image tag"
 }
 
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-2"
+resource "aws_ecs_task_definition" "task" {
+  family                   = "strapi-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "strapi"
+      image     = var.ecr_repo
+      essential = true
+
+      portMappings = [
+        { containerPort = 1337 }
+      ]
+    }
+  ])
 }
