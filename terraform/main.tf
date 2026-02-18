242019b6d13dc35977dabcbd7df0c4fbb3b9e1d8
Noorjahan Dudekula <90750848+Noorjahan153@users.noreply.github.com>
2026-02-18 20:20:24 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,6 @@ provider "aws" {
 }
 
 ################ VPC ################
-
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -28,16 +27,9 @@ resource "aws_subnet" "subnet2" {
   map_public_ip_on_launch = true
 }
 
-resource "aws_security_group" "strapi" {
+resource "aws_security_group" "strapi_sg" {
   name   = "strapi-sg"
   vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   ingress {
     from_port   = 1337
@@ -55,13 +47,11 @@ resource "aws_security_group" "strapi" {
 }
 
 ################ ECS CLUSTER ################
-
 resource "aws_ecs_cluster" "cluster" {
   name = "strapi-cluster"
 }
 
-################ IAM ROLE ################
-
+################ IAM EXECUTION ROLE ################
 resource "aws_iam_role" "ecs_execution_role" {
   name = "ecsTaskExecutionRole"
 
@@ -82,13 +72,53 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-################ ALB ################
+################ ECS TASK DEFINITION ################
+variable "ecr_repo" {}
 
-resource "aws_lb" "alb" {
+resource "aws_ecs_task_definition" "task" {
+  family                   = "strapi-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "strapi"
+      image     = var.ecr_repo
+      essential = true
+      portMappings = [
+        {
+          containerPort = 1337
+        }
+      ]
+    }
+  ])
+}
+
+################ ECS SERVICE ################
+resource "aws_ecs_service" "service" {
+  name            = "strapi-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+    security_groups = [aws_security_group.strapi_sg.id]
+    assign_public_ip = true
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.ecs_execution_policy]
+}
+
+################ ALB FOR PUBLIC URL ################
+resource "aws_lb" "strapi_alb" {
   name               = "strapi-alb"
-  internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.strapi.id]
+  security_groups    = [aws_security_group.strapi_sg.id]
   subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
 }
 
@@ -101,8 +131,8 @@ resource "aws_lb_target_group" "tg" {
 }
 
 resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = 80
+  load_balancer_arn = aws_lb.strapi_alb.arn
+  port              = "80"
   protocol          = "HTTP"
 
   default_action {
@@ -111,58 +141,8 @@ resource "aws_lb_listener" "listener" {
   }
 }
 
-################ ECS TASK ################
-
-variable "ecr_repo" {}
-
-resource "aws_ecs_task_definition" "task" {
-  family                   = "strapi-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
-  memory                   = "1024"
-
-  execution_role_arn = aws_iam_role.ecs_execution_role.arn
-
-  container_definitions = jsonencode([{
-    name      = "strapi"
-    image     = var.ecr_repo
-    essential = true
-
-    portMappings = [{
-      containerPort = 1337
-      hostPort      = 1337
-      protocol      = "tcp"
-    }]
-  }])
-}
-
-################ ECS SERVICE ################
-
-resource "aws_ecs_service" "service" {
-  name            = "strapi-service"
-  cluster         = aws_ecs_cluster.cluster.id
-  task_definition = aws_ecs_task_definition.task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets         = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
-    security_groups = [aws_security_group.strapi.id]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.tg.arn
-    container_name   = "strapi"
-    container_port   = 1337
-  }
-
-  depends_on = [aws_iam_role_policy_attachment.ecs_execution_policy, aws_lb_listener.listener]
-}
-
-################ OUTPUT ################
-
-output "strapi_url" {
-  value = aws_lb.alb.dns_name
+resource "aws_lb_target_group_attachment" "tg_attachment" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_ecs_service.service.network_configuration[0].subnets[0] # Fargate IPs are dynamic; ECS handles registration automatically
+  port             = 1337
 }
