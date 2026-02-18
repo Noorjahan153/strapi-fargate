d05e95c73d6e1d19e1fce8c739a68825fb984842
Noorjahan Dudekula <90750848+Noorjahan153@users.noreply.github.com>
2026-02-18 21:06:48 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-2"
+  region = var.region
 }
 
 ################ VPC ################
@@ -82,9 +82,11 @@ resource "aws_iam_role" "ecs_execution_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "ecs-tasks.amazonaws.com" }
-      Action    = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
     }]
   })
 }
@@ -96,31 +98,21 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
 
 ################ TASK DEFINITION ################
 
-variable "ecr_repo" {
-  description = "Full ECR repository URI including image tag"
-}
-
 resource "aws_ecs_task_definition" "task" {
   family                   = "strapi-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
   memory                   = "1024"
-  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
 
-  container_definitions = jsonencode([
-    {
-      name      = "strapi"
-      image     = var.ecr_repo
-      essential = true
+  execution_role_arn = aws_iam_role.ecs_execution_role.arn
 
-      portMappings = [
-        {
-          containerPort = 1337
-        }
-      ]
-    }
-  ])
+  container_definitions = jsonencode([{
+    name      = "strapi"
+    image     = var.ecr_repo
+    essential = true
+    portMappings = [{ containerPort = 1337 }]
+  }])
 }
 
 ################ ECS SERVICE ################
@@ -133,15 +125,8 @@ resource "aws_ecs_service" "service" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
-    security_groups = [aws_security_group.strapi.id]
+    subnets          = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+    security_groups  = [aws_security_group.strapi.id]
     assign_public_ip = true
   }
-}
-
-################ OUTPUT ################
-
-output "strapi_url" {
-  value = "http://${aws_ecs_service.service.network_configuration[0].subnets[0]}"
-  description = "Use this URL to access Strapi (public IP from subnet)"
 }
