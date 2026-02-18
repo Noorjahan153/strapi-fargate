334e5a819dd8db268eb6c2711f9616f0b38ced7d
Noorjahan Dudekula <90750848+Noorjahan153@users.noreply.github.com>
2026-02-18 22:20:22 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,8 @@
-# variables.tf
-
 variable "region" {
   default = "ap-south-2"
 }
 
 variable "ecr_repo" {
-  description = "Full ECR repository URI including image tag"
+  description = "Full ECR repository URI including image tag, e.g., 055013504553.dkr.ecr.ap-south-2.amazonaws.com/strapi:latest"
   type        = string
 }
