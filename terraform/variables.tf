8856c6280b95deed11ab1bb8389cf31532b3f98a
Noorjahan Dudekula <90750848+Noorjahan153@users.noreply.github.com>
2026-02-18 17:14:04 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,10 @@
-# variables.tf
-
-# AWS Account ID (12-digit number)
 variable "aws_account_id" {
-  description = "AWS Account ID for ECR repository"
+  description = "AWS account ID for ECR"
   type        = string
 }
 
-# AWS Region (optional, default to Hyderabad)
 variable "region" {
-  description = "AWS region for deployment"
+  description = "AWS region"
   type        = string
   default     = "ap-south-2"
 }
