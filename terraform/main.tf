82ea34a05ff3d4c0b3228ec8d0a7c15b505b2d65
Noorjahan Dudekula <90750848+Noorjahan153@users.noreply.github.com>
2026-02-18 13:13:14 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,28 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_ecs_cluster" "main" {
-  name = "strapi-cluster"
-}
-
-resource "aws_security_group" "sg" {
-  name   = "strapi-sg"
-  vpc_id = data.aws_vpc.default.id
-
-  ingress {
-    from_port = 1337
-    to_port   = 1337
-    protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+################ VPC + SUBNET ################
 
 data "aws_vpc" "default" {
   default = true
@@ -36,35 +15,92 @@ data "aws_subnets" "default" {
   }
 }
 
-resource "aws_ecs_task_definition" "task" {
-  family = "strapi-task"
-  cpu    = 512
-  memory = 1024
+################ SECURITY GROUP ################
 
-  network_mode = "awsvpc"
+resource "aws_security_group" "sg" {
+  name   = "strapi-sg"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+################ ECS CLUSTER ################
+
+resource "aws_ecs_cluster" "main" {
+  name = "strapi-cluster"
+}
+
+################ IAM EXECUTION ROLE (CRITICAL) ################
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+################ TASK DEFINITION ################
+
+resource "aws_ecs_task_definition" "task" {
+  family                   = "strapi-task"
+  cpu                      = 512
+  memory                   = 1024
+  network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
+
+  execution_role_arn = aws_iam_role.ecs_execution_role.arn
 
   container_definitions = jsonencode([
     {
-      name = "strapi"
-      image = var.ecr_repo
+      name      = "strapi"
+      image     = var.ecr_repo
       essential = true
+
       portMappings = [
-        { containerPort = 1337 }
+        {
+          containerPort = 1337
+        }
       ]
     }
   ])
 }
 
+################ ECS SERVICE ################
+
 resource "aws_ecs_service" "service" {
-  name = "strapi-service"
-  cluster = aws_ecs_cluster.main.id
+  name            = "strapi-service"
+  cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.task.arn
-  desired_count = 1
-  launch_type = "FARGATE"
+  desired_count   = 1
+  launch_type     = "FARGATE"
 
   network_configuration {
-    subnets = data.aws_subnets.default.ids
+    subnets         = data.aws_subnets.default.ids
     security_groups = [aws_security_group.sg.id]
     assign_public_ip = true
   }
