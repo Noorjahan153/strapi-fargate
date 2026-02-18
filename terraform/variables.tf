f449a180604065e8b001c0bc67ee21f8c10213ba
Noorjahan Dudekula <90750848+Noorjahan153@users.noreply.github.com>
2026-02-18 14:57:49 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,16 @@
 variable "region" {
-  default = "ap-south-2"
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "environment" {
+  description = "Deployment environment (dev, prod, test)"
+  type        = string
+  default     = "dev"
 }
 
 variable "ecr_repo" {
-  type = string
+  description = "ECR repository URI for the Strapi image"
+  type        = string
 }
